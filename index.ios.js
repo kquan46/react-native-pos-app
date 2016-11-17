6d3012314e92a8558ac578f5e75d2d667f1c7dd8
kquan46 <kevinquan46@hotmail.com>
2016-11-16 20:19:53 -0800

    added pushcode

--- index.ios.js
+++ index.ios.js
@@ -17,7 +17,7 @@ export default class NhaTrangPOS extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to React Native! lets try this
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.ios.js
