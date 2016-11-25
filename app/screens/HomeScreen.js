20c364b915c9b9cf16c7cdfd62250d7b16c6ac82
Kevin Quan <Kevin@Kevins-MacBook.local>
2016-11-24 17:14:28 -0800

    added populated data

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -7,6 +7,26 @@ import MainContainer from '../containers/MainContainer'
 import TableAndDeliveryContainer from '../containers/TableAndDeliveryContainer'
 import OrderContainer from '../containers/OrderContainer'
 
+const order_table = [
+  {
+    order_number: 1,
+    orderStatus: {numOfItems: 7, totalPrice: 98, payStatus: "notPaid", date: "2016/09/12", paidTime: "", startTime: "12:30"},
+    tableDetails: {tableNumber: 5, subTableNumber: "A", numOfPeople: 4},
+    orderDetails: [
+      {
+        foodItems: [
+          {name: "咖喱牛腩飯", customization: "少飯,走蒜", shortName: "喱反", type: "regularFood", quantity: 1, price: 37, orderTime: "12:35:01", payStatus: "notPaid", base: "飯", ingredients: {ingredient1: "咖喱牛腩", ingredient2: "", ingredient3: "", ingredient4: ""}},
+          {name: "香茅豬扒風沙雞翼撈檬", customization: "", shortName: "朱扒亦凍蒙", type: "regularFood", quantity: 2, price: 37, orderTime: "12:35:31", payStatus: "notPaid", base: "撈檬", ingredients: {ingredient1: "香茅豬扒", ingredient2: "風沙雞翼", ingredient3: "", ingredient4: ""}}
+        ],
+        drinkItems: [
+          {name: "凍檸檬茶", customization: "走甜,少冰", shortName: "凍OT", type: "mealDrink", quantity: 2, price: 2, orderTime: "12:35:40", payStatus: "notPaid", hotCold: "cold"},
+          {name: "青檸梳打冰", customization: "", shortName: "青O", type: "mealDrink", quantity: 1, price: 6, orderTime: "12:36:20", payStatus: "notPaid", hotCold: "cold"}
+        ]
+      }
+    ]
+  }
+]
+
 class HomeScreen extends Component {
   render() {
     return (
